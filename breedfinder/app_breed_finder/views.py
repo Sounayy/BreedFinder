4a89d5ecf198b03a1acb9e05b1ba4b01f0a8f0af
Nils Chabot <egekepyt@gmail.com>
2024-03-19 14:05:07 +0100

    Add template structure to the application

--- breedfinder/app_breed_finder/views.py
+++ breedfinder/app_breed_finder/views.py
@@ -1,18 +1,13 @@
-from django.http import HttpResponse
 from app_breed_finder.models import Breed
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def test(request):
     breeds = Breed.objects.all()
-    return HttpResponse(
-        f"""
-        <h1> BreedFinder </h1>
-        <h2> Mes races de chats préférées </h2>
-        <ul>
-            <li>{breeds[0].name}</li>
-            <li>{breeds[1].name}</li>
-        </ul>
-        """
+    return render(
+        request,
+        "app_breed_finder/test.html",
+        {"breeds": breeds},
     )

1d1620cd54039f0f4037f637975b4bd249e3519b
Nils Chabot <egekepyt@gmail.com>
2024-03-20 10:02:49 +0100

    Add several fields to Breed model

--- breedfinder/app_breed_finder/models.py
+++ breedfinder/app_breed_finder/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 class Breed(models.Model):
 
+    class Animal(models.TextChoices):
+        CAT = "CAT"
+        DOG = "DOG"
+
     name = models.fields.CharField(max_length=100)
+    animal = models.fields.CharField(choices=Animal.choices, max_length=50)
+    description = models.fields.TextField()
+    personality = models.fields.TextField()
+    origin = models.fields.TextField()
+    eating_habits = models.fields.TextField()

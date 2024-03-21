6d3ce8ac2253113cc5ed57c622627d4d1584e7dc
Nils Chabot <egekepyt@gmail.com>
2024-03-21 15:09:50 +0100

    Add life esperance attribute to breed model

--- breedfinder/app_breed_finder/models.py
+++ breedfinder/app_breed_finder/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Breed(models.Model):
@@ -13,6 +14,12 @@ class Breed(models.Model):
     personality = models.fields.TextField()
     origin = models.fields.TextField()
     eating_habits = models.fields.TextField()
+    life_esperance_bottom = models.fields.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(30)]
+    )
+    life_esperance_top = models.fields.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(30)]
+    )
 
     def __str__(self) -> str:
         return f"{self.name}"

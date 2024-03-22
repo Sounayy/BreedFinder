66127df739ceacebdc9da2b0f6074028fa0e9183
Nils Chabot <egekepyt@gmail.com>
2024-03-22 11:03:10 +0100

    Restructure url and template

--- breedfinder/app_breed_finder/views.py
+++ breedfinder/app_breed_finder/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def test(request):
+def home(request):
     breeds = Breed.objects.all()
     return render(
         request,
-        "app_breed_finder/test.html",
+        "app_breed_finder/home.html",
         {"breeds": breeds},
     )
--- breedfinder/breedfinder/urls.py
+++ breedfinder/breedfinder/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("test/", views.test),
+    path("", views.home),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

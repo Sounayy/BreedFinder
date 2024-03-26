a77f9db2f21a79c9484d002cba040abf64729b3b
Nils Chabot <egekepyt@gmail.com>
2024-03-26 11:03:48 +0100

    Add breed prediction logic to project

---
+++ breedfinder/app_breed_finder/utils/helpers.py
@@ -0,0 +1,10 @@
+from app_breed_finder.models import Breed
+
+
+def predicit_breed_from_image(image):
+    # ...
+    results = ["British ShortHair", "Bedlington"]
+    breeds_predicited_id = []
+    for result in results:
+        breeds_predicited_id.append(Breed.objects.get(name=result).id)
+    return breeds_predicited_id
--- breedfinder/app_breed_finder/views.py
+++ breedfinder/app_breed_finder/views.py
@@ -4,30 +4,34 @@ from app_breed_finder.forms import UploadImageForm
 import logging
 from django.shortcuts import redirect
 from django.urls import reverse
+from app_breed_finder.utils.helpers import predicit_breed_from_image
 
 
 def home(request):
     logger = logging.getLogger("mylogger")
     breeds = Breed.objects.all()
-    image = None
+    breeds_predicted_id = request.session.get("breeds_predicted_id", None)
+    if breeds_predicted_id is None:
+        breeds_predicted = breeds
+    else:
+        breeds_predicted = [
+            Breed.objects.get(id=breed_id) for breed_id in breeds_predicted_id
+        ]
     if request.method == "POST":
         form = UploadImageForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.cleaned_data["image_uploaded"]
-            context_data = {
-                "breeds": breeds,
-                "form": form,
-            }
-            redirect_url = reverse("home")
-            return redirect(redirect_url, context=context_data)
+            breeds_predicted_id = predicit_breed_from_image(image)
+            request.session["breeds_predicted_id"] = breeds_predicted_id
+            return redirect(reverse("home"))
     else:
         form = UploadImageForm()
-    context_data = {
-        "breeds": breeds,
-        "form": form,
-    }
     return render(
         request,
         "app_breed_finder/home.html",
-        context_data,
+        {
+            "breeds": breeds,
+            "form": form,
+            "breeds_predicted": breeds_predicted,
+        },
     )

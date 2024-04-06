0593bd15a4dea6dd2fb2c740d5ec2ba4d52c8924
Nils Chabot <egekepyt@gmail.com>
2024-04-06 16:33:10 +0200

    Add loading animation when a prediction is made

--- breedfinder/app_breed_finder/static/app_breed_finder/scripts/script.js
+++
@@ -1,6 +0,0 @@
-let animalSelect = document.getElementById('animal-select');
-
-animalSelect.addEventListener('change', function () {
-    let form = animalSelect.closest('form');
-    form.submit();
-});
